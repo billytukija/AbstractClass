48c6b252bd362e2024f8b2699626b4a63b574a83
Billy Tukija Richard <billytukija@gmail.com>
2019-10-03 09:57:43 -0300

    Desafio sobre Reflexão
    * Escrever uma nova opção na classe Escrever para receber qualquer objeto e retorne todas seus valores e propriedades.
         * Escrever um Metodo na class Program que execute o objeto Escrever logar de forma generica.

---
+++ ValueAndReferenceTypes/ValueAndReferenceTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ValueAndReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var type = typeof(TestClass);
+            Console.Write(type.Name);
+            Console.Read();
+        }
+
+        public class TestClass
+        {
+            public int Name { get; set; }
+        }
+    }
+}
