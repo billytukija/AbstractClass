e8887366a91e8d11f03c373ccf2386ba53c1dbf1
billytukija <31943520+billytukija@users.noreply.github.com>
2020-03-03 08:50:59 -0300

    Update Program.cs

--- AbsbtractClass/AbsbtractClass/Program.cs
+++ AbsbtractClass/AbsbtractClass/Program.cs
@@ -3,57 +3,44 @@
 namespace AbsbtractClass
 {
 
-    public abstract class Entity
-    {
-        public string Name { get; set; }
-        public long CreatedBy { get; set; }
-        public long UpdatedBy { get; set; }
-        public DateTime Created { get; set; }
-        public DateTime Updated { get; set; }
-        public string Description { get; set; }
-    }
-
-    /// <summary>
-    /// Implementing the class Entity, we gonna have all Entity properties in Product class
-    /// </summary>
-    public class Product : Entity
-    {
-        public double Price { get; set; }
-        public long ProductId { get; set; }
-        public DateTime ValidationDate { get; set; }
-    }
-
-
-    /// <summary>
-    /// Implementing the class Entity, we gonna have all Entity properties in Market class
-    /// </summary>
-    public class Market : Entity
-    {
-        public long MarketId { get; set; }
-        public string Type { get; set; }
-        public string Style { get; set; }
-    }
-
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            var product = new Product
-            {
-                Price = 15.8,
-                Name = "Coca-Cola",  // Notice that the property 'Name' is not dclared directly in the prodct class
-                Description = "American drinking product", // Notice that the property 'Description' is not dclared directly in the prodct class
-                Created = DateTime.Now.AddYears(-30), // Notice that the property 'Created' is not dclared directly in the prodct class
-                CreatedBy = 1, // Notice that the property 'CreatedBy' is not dclared directly in the prodct class
-                ValidationDate = DateTime.Now.AddYears(3)
-            };
+            var grazy = new Phisical();
+            grazy.Pay();
 
+            var beapps = new Legal();
+            beapps.Pay();
+        }
+    }
 
-            var market = new Market
-            {
-                Name = "Assaí",  // Notice that the property 'Name' is not dclared directly in the prodct class
-                // ...
-            };
+    public abstract class Person
+    {
+        public string Name { get; set; }
+
+        public virtual void Pay()
+        {
+        }
+    }
+
+    public class Phisical : Person
+    {
+        public string Cpf { get; set; }
+
+        public override void Pay()
+        {
+            Console.WriteLine("Paying by physical person");
+        }
+    }
+
+    public class Legal : Person
+    {
+        public string Cnpj { get; set; }
+
+        public override void Pay()
+        {
+            Console.WriteLine("Paying by legal person");
         }
     }
 }
